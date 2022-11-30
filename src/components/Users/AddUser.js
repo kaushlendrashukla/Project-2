cf6f93e465e347849800f352e2319a7e4860d66d
kaushlendrashukla <kaushlendrashukla2015@gmail.com>
2022-11-30 23:56:46 +0530

    task 5 JSX limitation and solution

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,12 +3,13 @@ import Card from "../UI/Card";
 import classes from './AddUser.module.css';
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 
 const AddUser = props => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
-    const [error, setError] =useState();
+    const [error, setError] = useState();
 
 
 
@@ -42,13 +43,13 @@ const AddUser = props => {
         setEnteredAge(event.target.value);
     };
 
-const errorHandler = () => {
-    setError(null);
-};
+    const errorHandler = () => {
+        setError(null);
+    };
 
     return (
-        <div>
-           {error &&  <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/> }
+        <Wrapper>
+            {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">Username</label>
@@ -58,7 +59,7 @@ const errorHandler = () => {
                     <Button type="submit">Add User </Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
     )
 };
 export default AddUser;
