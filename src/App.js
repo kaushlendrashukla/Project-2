5f9d57820ccec224f2f7ed802b7282d37853d541
kaushlendrashukla <kaushlendrashukla2015@gmail.com>
2022-12-05 22:59:24 +0530

    task6

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, {useState, Fragment} from 'react';
+import React, { useState, Fragment } from 'react';
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
 function App() {
   const [usersList, setUsersList] = useState([])
-const addUserHandler = (userName, userAge) => {
-  setUsersList(prevUsersList => {
-    return [...prevUsersList, {name:userName, age: userAge, id: Math.random().toString()}];
-  })
-}
+  const addUserHandler = (userName, userAge, userCollege) => {
+    setUsersList(prevUsersList => {
+      return [...prevUsersList, { name: userName, age: userAge, College : userCollege, id: Math.random().toString()  }];
+    })
+  }
   return (
     <Fragment>
-       <AddUser onAddUser = {addUserHandler}/>
-       <UsersList users={usersList}/>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </Fragment>
-    
+
   );
 }
 
--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -6,7 +6,7 @@ const UsersList = props => {
     <Card className = {classes.users}>
         <ul>
             {props.users.map(user =>(
-                <li key={user.id}>{user.name} {user.age}
+                <li key={user.id}> {user.name}  {user.college} {user.age}
                 </li>)
             )}
         </ul>
